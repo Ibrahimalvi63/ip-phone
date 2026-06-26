e3867127b9c99af82f345eb4d93d53ae40870aef
Ibrahim <ibrahimalvi63@gmail.com>
2026-06-26 10:48:21 +0600

    delete navbar

--- components/dialer/SettingsTab.js
+++ components/dialer/SettingsTab.js
@@ -115,7 +115,7 @@ export default function SettingsTab({ username, onLogout, onReinitDevice }) {
       {/* About */}
       <Section title="About">
         <Row label="Version" value="1.0.1" icon={<InfoIcon />} />
-        <Row label="Powered by" value="Twilio WebRTC" icon={<PhoneIcon />} />
+        <Row label="Powered by" value="NextWeb BD" icon={<PhoneIcon />} />
         <Row label="Developer" value={"SM IBRAHIM"} icon={<UserIcon />} />
       </Section>
 
